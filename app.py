157177372e0ce7a4a2eb849d6ee71d39ec3a807d
vikakkkkkkk <viktoriakucerova58@gmail.com>
2023-04-28 09:21:59 +0500

    rest

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from collections import Counter
 def anagrams(str1, str2):
     return Counter(str1) == Counter(str2)
-    anagrams("abc1" ,"1bac")
+anagrams("abc1" ,"1bac")
+print("Some print")
+    
